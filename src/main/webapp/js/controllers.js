06af6c015fba6aa1129b7f8b18d3896ce0f127c8
pnadolny <psnadolny@gmail.com>
2015-05-04 20:14:29 -0500

    Bootstap nav bar styling

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,48 +1,18 @@
 'use strict';
 
+angular.module('volleyballControllers', []).controller('VolleyballController',
+		function($scope, $log, $firebaseArray) {
 
-var volleyballAppControllers = angular.module('volleyballControllers', []);
-
-
-volleyballAppControllers.controller('VolleyballController',
-		function($scope, $log, vibrator, $firebaseArray) {
-
-  var ref = new Firebase("https://luminous-heat-7529.firebaseio.com/sets");
-$scope.match =  $firebaseArray(ref);
-
-	// $scope.match = Storage.loadObject('match');
-  $scope.hideVisitor = false;
-  $scope.vibrate = false;
-
-  $scope.vibrateSupport = vibrator.isSupported();
-
+  var ref = new Firebase("https://luminous-heat-7529.firebaseio.com/matches");
+  $scope.match =  $firebaseArray(ref);
 	$scope.$watch('match',	function(newValue,oldValue) {
-				if (vibrator.isSupported() && $scope.vibrate) {
-					vibrator.vibrate(100);
-				}
 				for (var x = 0; x < $scope.match.length; x++) {
 					$scope.match.$save($scope.match[x]);
 		    }
 			},true
-
 	);
+  $scope.compose = function() {
+      $scope.match.$add({'homeScore': 0,'visitorScore': 0});
+  }
 
-    $scope.compose = function() {
-
-	    	var set = {
-            'homeScore': 0,
-            'visitorScore': 0,
-        }
-
-        $scope.match.$add(set);
-    }
-
-
-    $scope.erase = function() {
-				$scope.match.$remove(0,$scope.match.length);
-    }
-
-    
-
-
-  });
+});
