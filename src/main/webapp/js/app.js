244dc2d72145fed01c48b993527700cbcbaf85f9
Paul Nadolny <psnadolny@gmail.com>
2015-05-11 15:22:38 -0500

    Starting on ngRouting and then ng-view soon

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,15 @@
 'use strict';
 
 // Register modules for the app
-angular.module("volleyballApp", ['volleyballControllers','ngMaterial','firebase']);
+angular
+.module("volleyballApp", ['volleyballControllers','ngMaterial','firebase','ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+   .when('/scoreboard', {
+   templateUrl: '/scoreboard.html'
+  })
+  .otherwise({
+      redirectTo: '/index.html'
+  });
+
+});
