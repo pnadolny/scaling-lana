c45f51d4ebc3c7fbba21d0fa024bae97b082ba96
pnadolny <psnadolny@gmail.com>
2015-05-18 18:08:27 -0500

    Add SignOut..and check if signed in already

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -3,15 +3,55 @@
 angular.module('volleyballControllers', []).controller('VolleyballController',
 		function($scope, $log, $firebaseArray,$mdDialog,$mdToast) {
 
+			$scope.showSimpleToast = function(message) {
+			    $mdToast.show(
+			      $mdToast.simple()
+			        .content(message)
+			        .hideDelay(3000)
+			    );
+			  };
+
+
+
 	$scope.authenticatedDisplayName =null;
 	$scope.authenticated =false;
+	$scope.authData = null;
 
   var ref = new Firebase("https://luminous-heat-7529.firebaseio.com/matches");
+
+	ref.onAuth(authDataCallback);
+  ref.getAuth();
+
+
 	$scope.match =  $firebaseArray(ref);
+
+	// Create a callback which logs the current auth state
+	function authDataCallback(authData) {
+	  if (authData) {
+			$scope.authenticated =true;
+			$scope.authData = authData;
+			if (authData.provider ==='google') {
+				$scope.authenticatedDisplayName = authData.google.displayName;
+			} else {
+				$scope.authenticatedDisplayName = authData.twitter.displayName;
+			}
+			$scope.showSimpleToast($scope.authenticatedDisplayName + " is logged in via provider " + authData.provider);
+	  } else {
+			$scope.authenticated =false;
+			$scope.authenticatedDisplayName = null;
+			$scope.showSimpleToast("You are currently logged out.");
+	  }
+	}
 
 
   $scope.compose = function() {
-      $scope.match.$add({'homeScore': 0,'visitorScore': 0,'author': $scope.authenticatedDisplayName, 'status':'1', 'date': Date.now()});
+      $scope.match.$add({
+			'homeScore': 0,
+			'visitorScore': 0,
+			'author': $scope.authenticatedDisplayName,
+			'status':'1',
+			'date': Date.now(),
+			'uid':$scope.authData.uid});
   }
 
 	$scope.delete = function(ev, set) {
@@ -22,13 +62,11 @@ angular.module('volleyballControllers', []).controller('VolleyballController',
 			}, function() {});
 	}
 
-		$scope.showSimpleToast = function(message) {
-		    $mdToast.show(
-		      $mdToast.simple()
-		        .content(message)
-		        .hideDelay(3000)
-		    );
-		  };
+	$scope.signOut = function(){
+	 	ref.unauth();
+
+	}
+
 
 		$scope.signIn = function(provider){
 
